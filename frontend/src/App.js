673d8807a135c0e4847061d535a6927b7c827fab
the3venthoriz0n <andrewkpln@gmail.com>
2025-11-01 17:47:46 -0700

    Add logo and favicon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -345,7 +345,7 @@ function App() {
 
   return (
     <div className="app">
-      <h1>🎥 Hopper</h1>
+      <h1>🐸 Hopper</h1>
       
       {message && <div className="message">{message}</div>}
       
